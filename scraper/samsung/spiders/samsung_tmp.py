d20ec7ccebcdd920a9584e0046815d8924270ec2
Cho Hyunwoo <chodima0@gmail.com>
2024-03-17 21:38:41 +0900

    refactor(scraper): initiating API reference scraping algorithm

--- scraper/samsung/spiders/samsung_tmp.py
+++ scraper/samsung/spiders/samsung_tmp.py
@@ -1,127 +1,157 @@
 import scrapy
-from playwright.async_api import Page
+import json
 
 
 class SamsungSpider(scrapy.Spider):
     name = "samsung_tmp"
     allowed_domains = ["www.samsung.com"]
-    start_urls = [
-	    # "https://www.samsung.com/sec/smartphones/all-smartphones/",
-	    # "https://www.samsung.com/sec/tablets/all-tablets/",
-	    # "https://www.samsung.com/sec/water-purifier/all-water-purifier/",
-	    # "https://www.samsung.com/sec/electric-range/all-electric-range/",
-	    # "https://www.samsung.com/sec/kitchen-small-appliance/all-kitchen-small-appliance/",
-	    # "https://www.samsung.com/sec/washing-machines/all-washing-machines/",
-	    "https://www.samsung.com/sec/dryers/all-dryers/",
-	    # "https://www.samsung.com/sec/airdresser/all-airdresser/",
-	    # "https://www.samsung.com/sec/air-conditioners/all-air-conditioners/",
-	    # "https://www.samsung.com/sec/smartthings-accessories/all-smartthings-accessories/",
-    ]
+    tmp_url_prefix = "https://www.samsung.com/sec/"
+    template_url = "https://www.samsung.com/sec/pf/goodsList?dispClsfNo={}&page=1&rows=1&ehcacheYn=Y&soldOutExceptYn=N&pfFasterUseYn=Y&secApp=false"
+    category_to_classification_number = {
+        "all-smartphones":             33010000,
+        "all-tablets":                 33020000,
+        "all-galaxybook":              39120000,
+        "all-watches":                 33110000,
+        "all-buds":                    33120000,
+        "all-mobile-accessories":      33050000,
+        "all-tvs":                     34060000,
+        "all-lifestyletv":             34080000,
+        "all-samsung-audio":           41040000,
+        "all-harman-life-style-audio": 41090000,
+        "all-tv-accessories":          34090000,
+        "all-refrigerators":           36010000,
+        "all-kimchi-refrigerators":    36020000,
+        "all-dishwashers":             36080000,
+        "all-water-purifier":          100020195,
+        "all-electric-range":          36070000,
+        "all-cooking-appliances":      36030000,
+        "all-micro-wave-ovens":        100034611,
+        "all-hood":                    100027453,
+        "all-kitchen-small-appliance": 100037843,
+        "all-kitchen-accessories":     36060000,
+        "all-washing-machines":        37020000,
+        "all-dryers":                  37080000,
+        "all-airdresser":              37090000,
+        "all-shoedresser":             100020234,
+        "all-air-conditioners":        37010000,
+        "all-system-air-conditioners": 100024278,
+        "all-air-cleaner":             37040000,
+        "all-vacuum-cleaners":         37030000,
+        "all-small-appliances":        37050000,
+        "all-living-accessories":      37070000,
+        "all-desktop":                 39020000,
+        "all-pc-accessories":          39060000,
+        "all-monitors":                39030000,
+        "all-printers":                39040000,
+        "all-printer-supplies":        39070000,
+        "all-memory-storage":          40030000,
+        "all-smartthings-accessories": 100024735,
+    }
 
     def start_requests(self):
-        for products_list_url in self.start_urls:
+        for product_category, classification_number in self.category_to_classification_number.items():
+            self.start_urls.append(self.template_url.format(classification_number))
+
             yield scrapy.Request(
-                url=products_list_url,
-                meta={
-                    "playwright": True,
-                    "playwright_include_page": True,
-                    # "playwright_context": "product_lists",
-                },
+                url=self.start_urls[-1],
+                meta={ 'product-category': product_category },
                 callback=self.parse,
             )
 
-    async def load_all_items(self, page: Page):
-        from lxml.etree import HTMLParser, fromstring as html_from_string
+    def parse(self, response: scrapy.http.Response):
+        response_json = json.loads(response.body)
+        response_json = response_json.get('products')[0]
+        new_json = {}
 
-        await page.wait_for_selector("button#morePrd")
-        amount_of_sets = int(await page.locator("span#totalPageCount").text_content())
-        
-        item_number = 11
-        for _ in range(amount_of_sets):
-            await page.wait_for_selector(f"li.item:nth-child({item_number})")
-            await page.evaluate("morePrd()")
-            item_number += 12
+        for key, value in response_json.items():
+            if value is None:
+                continue
+            elif type(value) == dict:
+                new_json1 = {}
+                for key1, value1 in value.items():
+                    if value1 is None:
+                        continue
+                    else:
+                        pass
+                        new_json1.update({key1: value1})
+                new_json.update({key: new_json1})
+            else:
+                new_json.update({key: value})
 
-        html = html_from_string(await page.locator("ul.list-type").inner_html(), HTMLParser())
+        yield new_json
+    # async def parse(self, response: scrapy.http.Response):
+    #     html, amount_of_sets = await self.load_all_items(response.meta["playwright_page"])
 
-        await page.close()
-        await page.context.close()
+    #     for n, card_url_selector in enumerate(html.cssselect("li.item a.link-review"), start=1):
+    #         card_url_postfix = card_url_selector.attrib["href"]
+    #         card_url_postfix = card_url_postfix.rstrip("?focus=review")
+    #         card_url = response.urljoin(card_url_postfix)
 
-        return html, amount_of_sets
-       
-    async def parse(self, response: scrapy.http.Response):
-        html, amount_of_sets = await self.load_all_items(response.meta["playwright_page"])
+    #         context_number = n % amount_of_sets
+    #         yield scrapy.Request(
+    #             url=card_url,
+    #             meta={ "playwright": True },
+    #             callback=self.parse_card,
+    #         )
 
-        for n, card_url_selector in enumerate(html.cssselect("li.item a.link-review"), start=1):
-            card_url_postfix = card_url_selector.attrib["href"]
-            card_url_postfix = card_url_postfix.rstrip("?focus=review")
-            card_url = response.urljoin(card_url_postfix)
+    # async def parse_card(self, response: scrapy.http.Response):
+    #     prices_selector = response.css("div.itm-price")
+    #     props_selector = response.css("div.itm-option-choice")
 
-            context_number = n % amount_of_sets
-            yield scrapy.Request(
-                url=card_url,
-                meta={ "playwright": True },
-                callback=self.parse_card,
-            )
+    #     return {
+    #         "title": response.css("h1#goodsDetailNm::text").get(),
+    #         "url": response.url,
+    #         "model": response.css("div.itm-sku::text").get(),
+    #         "category": self.name,
 
-    async def parse_card(self, response: scrapy.http.Response):
-        prices_selector = response.css("div.itm-price")
-        props_selector = response.css("div.itm-option-choice")
+    #         # "benefit_price_validity_period": response.css().get(),
+    #         # "coupon_discount_validity_period": response.css().get(),
 
-        return {
-            "title": response.css("h1#goodsDetailNm::text").get(),
-            "url": response.url,
-            "model": response.css("div.itm-sku::text").get(),
-            "category": self.name,
+    #         "rating": response.css("div.itm-sart-rating span::text").get(),
+    #         "quantity_of_reviews": response.css("a.itm-review-count::text").get(),
+    #         **self.parse_prices(prices_selector),
+    #         **self.parse_additional_props(props_selector),
+    #     }
 
-            # "benefit_price_validity_period": response.css().get(),
-            # "coupon_discount_validity_period": response.css().get(),
+    # def parse_prices(self, prices_selector: scrapy.Selector):
+    #     prices = {
+    #         "standard_price": None,
+    #         "member_price": None,
+    #         "benefit_price": None,
+    #         "outlet_special_price": None,
+    #         "coupon_discount_quantity": None,
+    #         "coupon_discounted_price": None,
+    #     }
+    #     extract_quantity = lambda pb: pb.css("dd span::text").get().strip()
 
-            "rating": response.css("div.itm-sart-rating span::text").get(),
-            "quantity_of_reviews": response.css("a.itm-review-count::text").get(),
-            **self.parse_prices(prices_selector),
-            **self.parse_additional_props(props_selector),
-        }
+    #     for price_selector in prices_selector.css("dl"):
+    #         self.log(price_selector.css("dt::text").get().strip())
+    #         match price_selector.css("dt::text").get().strip():
+    #             case "기준가":
+    #                 prices["standard_price"] = extract_quantity(price_selector)
+    #             case "회원가":
+    #                 prices["member_price"] = extract_quantity(price_selector)
+    #             case "혜택가":
+    #                 prices["benefit_price"] = extract_quantity(price_selector)
+    #             case "아울렛 특가":
+    #                 prices["outlet_special_price"] = extract_quantity(price_selector)
+    #             case "쿠폰 할인 금액":
+    #                 prices["coupon_discount_quantity"] = extract_quantity(price_selector)
+    #             case "쿠폰 적용 예상가":
+    #                 prices["coupon_discounted_price"] = extract_quantity(price_selector)
+    #             case other:
+    #                 self.logger.warn(f"'{other}' is not defined by the parser")
 
-    def parse_prices(self, prices_selector: scrapy.Selector):
-        prices = {
-            "standard_price": None,
-            "member_price": None,
-            "benefit_price": None,
-            "outlet_special_price": None,
-            "coupon_discount_quantity": None,
-            "coupon_discounted_price": None,
-        }
-        extract_quantity = lambda pb: pb.css("dd span::text").get().strip()
+    #     return prices
 
-        for price_selector in prices_selector.css("dl"):
-            self.log(price_selector.css("dt::text").get().strip())
-            match price_selector.css("dt::text").get().strip():
-                case "기준가":
-                    prices["standard_price"] = extract_quantity(price_selector)
-                case "회원가":
-                    prices["member_price"] = extract_quantity(price_selector)
-                case "혜택가":
-                    prices["benefit_price"] = extract_quantity(price_selector)
-                case "아울렛 특가":
-                    prices["outlet_special_price"] = extract_quantity(price_selector)
-                case "쿠폰 할인 금액":
-                    prices["coupon_discount_quantity"] = extract_quantity(price_selector)
-                case "쿠폰 적용 예상가":
-                    prices["coupon_discounted_price"] = extract_quantity(price_selector)
-                case other:
-                    self.logger.warn(f"'{other}' is not defined by the parser")
+    # def parse_additional_props(self, props_selector: scrapy.Selector):
+    #     props = {}
 
-        return prices
+    #     for prop_selector in props_selector.css("dl")[:-1]:
+    #         if prop_selector.attrib["class"] not in ("count-show-box", "itm-option-etc"):
+    #             prop_title = prop_selector.css("dt span::text").get()
+    #             prop_values = prop_selector.css("dd li span::text").getall()
 
-    def parse_additional_props(self, props_selector: scrapy.Selector):
-        props = {}
+    #             props[prop_title] = prop_values
 
-        for prop_selector in props_selector.css("dl")[:-1]:
-            if prop_selector.attrib["class"] not in ("count-show-box", "itm-option-etc"):
-                prop_title = prop_selector.css("dt span::text").get()
-                prop_values = prop_selector.css("dd li span::text").getall()
-
-                props[prop_title] = prop_values
-
-        return props
+    #     return props
