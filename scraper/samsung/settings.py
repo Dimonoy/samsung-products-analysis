8725e5828653ba439b7f2e5818dc175f858aad8c
Cho Hyunwoo <chodima0@gmail.com>
2024-04-07 20:34:46 +0900

    build(scraper): adding MySQL configuration settings and pipelines to the settings.py

--- scraper/samsung/settings.py
+++ scraper/samsung/settings.py
@@ -1,3 +1,11 @@
+import pathlib
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 BOT_NAME = "samsung"
 
 SPIDER_MODULES = ["samsung.spiders"]
@@ -18,3 +26,15 @@ HTTPCACHE_GZIP = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+ITEM_PIPELINES = {
+    "samsung.pipelines.PricesPipeline": 300,
+    "samsung.pipelines.MySQLPipeline": 900,
+}
+
+MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_TABLE = os.getenv("MYSQL_TABLE")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
