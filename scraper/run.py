db2a33b3fe71e0a33a1ec46c802087d0dae788ad
Cho Hyunwoo <chodima0.e@gmail.com>
2024-05-06 13:49:03 +0900

    feat(scraper): adding diagnosing function in run.py script to see the difference between the size of the output data file in comparison to the one before

--- scraper/run.py
+++ scraper/run.py
@@ -1,9 +1,28 @@
+import os
+import subprocess
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+IBD_PATH = os.environ['IBD_PATH']
+
+def diagnose() -> str:
+    command = f'du -sh {IBD_PATH}'
+    return subprocess.check_output(command,
+                                   shell=True,
+                                   text=True)
 
 
 if __name__ == '__main__':
+    diagnose_results_before: str = diagnose()
+
     process = CrawlerProcess(settings=get_project_settings())
 
     process.crawl(crawler_or_spidercls="samsung")
     process.start()
+
+    diagnose_results_after: str = diagnose()
